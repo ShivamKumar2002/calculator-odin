4d50cfb11fc3cdb730a559668ee6d983489adbaa
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-11-07 22:24:29 +0530

    Make Basic Operation Functions and Make UI of Calculator Screen
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

---
+++ script.js
@@ -0,0 +1,38 @@
+const liveResultDiv = document.getElementById("liveResult");
+const inputAreaDiv = document.getElementById("inputArea");
+
+
+function add(a, b) {
+    return a + b;
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a, b)
+        case "-":
+            return subtract(a - b)
+        case "*":
+            return multiply(a, b)
+        case "/":
+            return divide(a, b)
+        default:
+            return "Unknown Operator"
+    }
+}
